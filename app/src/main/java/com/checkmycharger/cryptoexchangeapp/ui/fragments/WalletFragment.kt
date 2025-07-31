72d96da27be3a228fddee925166f841f29d7d7a7
Mohd Raghib <raghib.khan76@gmail.com>
2025-08-01 01:11:18 +0530

    add WalletFragment.kt

---
+++ app/src/main/java/com/checkmycharger/cryptoexchangeapp/ui/fragments/WalletFragment.kt
@@ -0,0 +1,28 @@
+package com.checkmycharger.cryptoexchangeapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.checkmycharger.cryptoexchangeapp.databinding.FragmentWalletBinding
+
+class WalletFragment : Fragment() {
+
+    private var _binding: FragmentWalletBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentWalletBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
