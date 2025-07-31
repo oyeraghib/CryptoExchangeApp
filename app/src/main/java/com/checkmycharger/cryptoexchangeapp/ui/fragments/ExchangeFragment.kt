0c8a0920a5b5fafbe378977a09de7f2e5901ac77
Mohd Raghib <raghib.khan76@gmail.com>
2025-08-01 01:09:16 +0530

    add onDestroyView in ExchangeFragment.kt

--- app/src/main/java/com/checkmycharger/cryptoexchangeapp/ui/fragments/ExchangeFragment.kt
+++ app/src/main/java/com/checkmycharger/cryptoexchangeapp/ui/fragments/ExchangeFragment.kt
@@ -20,4 +20,9 @@ class ExchangeFragment : Fragment() {
         _binding = FragmentExchangeBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
