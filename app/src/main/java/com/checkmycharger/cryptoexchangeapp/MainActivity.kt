5aee932c973eaa9ab4e7540f36be96d447c727bd
Mohd Raghib <raghib.khan76@gmail.com>
2025-07-31 16:19:30 +0530

    inflate MainActivity.kt using view binding

--- app/src/main/java/com/checkmycharger/cryptoexchangeapp/MainActivity.kt
+++ app/src/main/java/com/checkmycharger/cryptoexchangeapp/MainActivity.kt
@@ -3,18 +3,20 @@ package com.checkmycharger.cryptoexchangeapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.checkmycharger.cryptoexchangeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
