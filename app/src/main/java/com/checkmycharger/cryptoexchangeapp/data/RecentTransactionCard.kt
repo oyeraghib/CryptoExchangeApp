ffce377ccdf6b7c544b2c593176293366f7ae1b5
Mohd Raghib <raghib.khan76@gmail.com>
2025-08-01 20:31:38 +0530

    add RecentTransactionCard.kt

---
+++ app/src/main/java/com/checkmycharger/cryptoexchangeapp/data/RecentTransactionCard.kt
@@ -0,0 +1,9 @@
+package com.checkmycharger.cryptoexchangeapp.data
+
+data class RecentTransactionCard(
+    val imageResId: Int,
+    val type: String,
+    val date: String,
+    val coinShortName: String,
+    val amount: String
+)
