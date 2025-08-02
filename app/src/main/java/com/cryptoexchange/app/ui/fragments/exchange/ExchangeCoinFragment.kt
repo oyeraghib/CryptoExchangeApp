ae7e9857c49b5e1b665dd58eeeb38672e9c58fd5
Mohd Raghib <raghib.khan76@gmail.com>
2025-08-02 12:43:20 +0530

    fix: show round corners for exchange cards

--- app/src/main/java/com/cryptoexchange/app/ui/fragments/exchange/ExchangeCoinFragment.kt
+++ app/src/main/java/com/cryptoexchange/app/ui/fragments/exchange/ExchangeCoinFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.cryptoexchange.app.R
 import com.cryptoexchange.app.databinding.FragmentExchangeCoinBinding
 
 class ExchangeCoinFragment : Fragment() {
@@ -25,4 +28,28 @@ class ExchangeCoinFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupCardCorners()
+    }
+
+    /**
+     * Set up the card corners dynamically. The top card have upper round corners and the bottom
+     * card have lower rounded corners
+     */
+    private fun setupCardCorners() {
+        val topCard = binding.cardContainer.getChildAt(0)
+
+        val bottomCard = binding.cardContainer.getChildAt(1)
+
+        topCard.findViewById<View>(R.id.cardRoot).background =
+            ContextCompat.getDrawable(requireContext(), R.drawable.bg_card_top_round)
+
+        bottomCard.findViewById<View>(R.id.cardRoot).background =
+            ContextCompat.getDrawable(requireContext(), R.drawable.bg_card_bottom_round)
+
+    }
+
 }
