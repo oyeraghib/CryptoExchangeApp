20f9d78a5318ee0fea493fc8bd4c9a289cc7c917
Mohd Raghib <raghib.khan76@gmail.com>
2025-08-02 21:58:08 +0530

    update CoinInfo.kt and add mock data

--- app/src/main/java/com/cryptoexchange/app/data/CoinInfo.kt
+++ app/src/main/java/com/cryptoexchange/app/data/CoinInfo.kt
@@ -1,8 +1,17 @@
 package com.cryptoexchange.app.data
 
+import com.cryptoexchange.app.R
+
 data class CoinInfo(
-    val iconResId: Int,
     val name: String,
+    val iconResId: Int,
     val balance: String,
-    val rate: String
+    val amount: String
 )
+
+val coinMap = mapOf(
+    "ETH" to CoinInfo("ETH", R.drawable.ic_eth_png, "0.542", "1.5"),
+    "BTC" to CoinInfo("BTC", R.drawable.ic_btc_png, "0.125", "2.424"),
+    "INR" to CoinInfo("INR", R.drawable.ic_inr_currency, "₹21,230", "3,53,135.07")
+)
+
